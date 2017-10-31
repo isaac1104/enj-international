6112f1eedd7577beccd99e7221999c446727a268
Isaac Kwon <you@example.com>
2017-10-30 19:58:55 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+
+  $("#map").attr("src", `https://www.google.com/maps/embed/v1/search?key=AIzaSyCbRPJ4WPt885Uvh3ORlze25kJN6VsSE4A&q=13079 Artesia Blvd Cerritos, CA 90703`);
+
+});
